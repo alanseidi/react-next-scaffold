1d483d2aed6262f5e8d425f273f23c38539d5f00
alan <seidihashimura@gmail.com>
2021-09-14 19:15:48 -0300

    [UPD] Correção do head do _document.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Head from "next/head";
 import { Container } from "../styles/pages/Index";
 
 const Home: React.FC = () => (
   <Container>
+    <Head>
+      <title>App React With NextJs</title>
+    </Head>
     <main>
       <h1>Hello Word</h1>
     </main>
